9eae0c4fd21d7a8ef14b4d0d1473becb023e22a0
spreet2472 <samre2472@gmail.com>
2020-09-29 11:14:11 -0400

    input

---
+++ src/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <div>
+      <h2>Enter a To-Do item</h2>
+      <input type='text' placeholder='Enter a Task' />
+    </div>
+  );
+};
+
+export default Input;
